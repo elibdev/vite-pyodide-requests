2e8502e26fecf209dc3b55bcb832ddd31cf2f5a6
elibdev <eli@elib.dev>
2024-05-28 19:32:42 -0400

    add reproducible test case

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { onMounted } from "vue";
+import { loadPyodide } from "pyodide";
+
+onMounted(async () => {
+    const pyodide = await loadPyodide({ packages: ["requests"] });
+    console.log(pyodide.runPython("1 + 1"));
+});
+</script>
+
+<template>
+    <h1>Check browser console for output / errors.</h1>
+</template>
