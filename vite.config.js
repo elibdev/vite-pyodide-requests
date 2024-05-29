e5b9c9d2905e84a3f2816bf6ed90a60ec54d2b42
elibdev <eli@elib.dev>
2024-05-29 05:19:30 -0400

    add custom logger to ignore warnings from within pyodide

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
-import { defineConfig } from "vite";
+import { createLogger, defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+
+const logger = createLogger();
+const loggerWarn = logger.warn;
+
+logger.warn = (msg, options) => {
+  // Ignore warnings from pyodide distribution
+  if (msg.includes("/public/pyodide/")) return;
+  loggerWarn(msg, options);
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  customLogger: logger,
   optimizeDeps: { exclude: ["pyodide"] },
 });
