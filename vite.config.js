09560722e8b0c426bc1dd1d4dbe31ff3621a149c
elibdev <eli@elib.dev>
2024-05-28 22:39:37 -0400

    try serving pyodide from assets folder

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,33 @@
-import { defineConfig } from "vite";
+import { createLogger, defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+
+const logger = createLogger();
+const loggerWarn = logger.warn;
+
+logger.warn = (msg, options) => {
+  // Ignore warnings from pyodide distribution
+  if (msg.includes("/public/pyodide/")) return;
+  loggerWarn(msg, options);
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  assetsInclude: ["**/*.md"],
+  // resolve: {
+  //   alias: {
+  //     pyodide: "./public/pyodide/pyodide.mjs",
+  //   },
+  // },
   optimizeDeps: { exclude: ["pyodide"] },
+  customLogger: logger,
+  // build: {
+  //   rollupOptions: {
+  //     external: [
+  //       "pyodide",
+  //       "./public/pyodide/",
+  //       // /node_modules/,
+  //     ],
+  //   },
+  // },
 });
